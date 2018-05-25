9130d7925271d2aa4221dbcd0e53fc76f7510b78
Lauren Chambers <lchambers@stsci.edu>
2018-05-25 13:27:12 -0400

    Add classes for research staff data & renewal and promotion data

--- read_data.py
+++ read_data.py
@@ -18,14 +18,19 @@ References
     Data formatted to match Bokeh bar plot example, located here:
     http://bokeh.pydata.org/en/latest/docs/gallery/bar_nested_colormapped.html
 
+
 Notes
 -----
     Created as a part of STScI "Hack the 'Tute" Day, May 25, 2018
 '''
 
+import os
+
 import numpy as np
 from astropy.io import ascii as asc
 from bokeh.models import ColumnDataSource
+
+DATA_DIR = os.path.abspath('data')
 
 
 class SRC_data():
@@ -33,7 +38,8 @@ class SRC_data():
     '''
     def __init__(self):
         # Load data from CSV file
-        self.data = asc.read('data/SRC_stats_2012.csv')
+        file = os.path.join(DATA_DIR, 'SRC_stats_2012.csv')
+        self.data = asc.read(file)
 
         # Define axes
         self.years = ['2012', '2014', '2016']
@@ -44,12 +50,16 @@ class SRC_data():
 
     def get_data(self, stage):
         # Possible stages include 'pool', 'long-list', 'short-list', 'hire'
+        if stage not in ['pool', 'long-list', 'short-list', 'hire']:
+            return ValueError("Invalid state {}; must be one of 'pool', 'long-list', 'short-list', 'hire'".format(stage))
         self.data = {'years': self.years,
                      'Male': self.data['{} M'.format(stage)],
-                     'Female': self.data['{} M'.format(stage)],
-                     'Non-Binary': [np.nan]*3}
+                     'Female': self.data['{} F'.format(stage)],
+                     'Non-Binary': [0]*3}
 
     def create_columns(self):
+        '''Create ColumnDataSource for nested Bokeh bar plot
+        '''
         # Create an hstack of the data
         counts = sum(zip(self.data['Male'],
                          self.data['Female'],
@@ -64,7 +74,8 @@ class science_evaluation_data():
     '''
     def __init__(self):
         # Load data from CSV file
-        self.data = asc.read('data/Science_Evals_SEC.csv')
+        file = os.path.join(DATA_DIR, 'Science_Evals_SEC.csv')
+        self.data = asc.read(file)
 
         # Define axes
         self.years = ['2013', '2014', '2015', '2016', '2017']
@@ -75,12 +86,83 @@ class science_evaluation_data():
 
     def get_data(self, level):
         # Levels range from 1 to 4
+        if str(level) not in ['1', '2', '3', '4']:
+            return ValueError("Invalid state {}; must be between 1 and 4".format(level))
         self.data = {'years': self.years,
                      'Male': self.data['Level {} Male'.format(level)],
                      'Female': self.data['Level {} Female'.format(level)],
-                     'Non-Binary': [np.nan]*5}
+                     'Non-Binary': [0]*5}
 
     def create_columns(self):
+        '''Create ColumnDataSource for nested Bokeh bar plot
+        '''
+        # Create an hstack of the data
+        counts = sum(zip(self.data['Male'],
+                         self.data['Female'],
+                         self.data['Non-Binary']),
+                     ())
+        source = ColumnDataSource(data=dict(x=self.x, counts=counts))
+        return source
+
+
+class research_staff_data():
+    '''Load and format data about research staff
+    '''
+    def __init__(self):
+        # Load data from CSV file
+        file = os.path.join(DATA_DIR, 'Research_staff_stats.csv')
+        self.data = asc.read(file)
+
+        # Define axes
+        self.years = np.arange(1980, 2018).astype(str)
+        self.genders = ['Male', 'Female', 'Non-Binary']
+
+        # Create tuples for every combination of year and gender
+        self.x = [(year, gender) for year in self.years for gender in self.genders]
+
+    def get_data(self, data_type):
+        # Possible types include 'Hire' and 'Left'
+        if data_type not in ['Hire', 'Left']:
+            return ValueError("Invalid type {}; must be either 'Hire' or 'Left'".format(data_type))
+        self.data = {'years': self.years,
+                     'Male': self.data['{} male'.format(data_type)],
+                     'Female': self.data['{} female'.format(data_type)],
+                     'Non-Binary': [0]*5}
+
+    def create_columns(self):
+        '''Create ColumnDataSource for dodged Bokeh bar plot
+        '''
+        source = ColumnDataSource(data=self.data)
+        return source
+
+
+class renewal_promotion_data():
+    '''Load and format data about renewal and promotions
+    '''
+    def __init__(self):
+        # Load data from CSV file
+        file = os.path.join(DATA_DIR, 'Renewal_Promotion_SPC.csv')
+        self.data = asc.read(file)
+
+        # Define axes
+        self.years = ['2013', '2014', '2015', '2016', '2017']
+        self.genders = ['Male', 'Female', 'Non-Binary']
+
+        # Create tuples for every combination of year and gender
+        self.x = [(year, gender) for year in self.years for gender in self.genders]
+
+    def get_data(self, data_type):
+        # Possible types include 'Renewal', 'Promotion', and 'Cases'
+        if data_type not in ['Renewal', 'Promotion', 'Cases']:
+            return ValueError("Invalid type {}; must be either 'Renewal', 'Promotion', or 'Cases'".format(data_type))
+        self.data = {'years': self.years,
+                     'Male': self.data['{} Male'.format(data_type)],
+                     'Female': self.data['{} Female'.format(data_type)],
+                     'Non-Binary': [0]*5}
+
+    def create_columns(self):
+        '''Create ColumnDataSource for nested Bokeh bar plot
+        '''
         # Create an hstack of the data
         counts = sum(zip(self.data['Male'],
                          self.data['Female'],
